1bfc47be7fc95b336f6c6eccba67d7940e3c07de
ForgottenUmbrella <forgottenumbrella@tutanota.com>
2020-04-10 20:39:53 +1000

    Learn to echo battery status to stdout

--- battery-monitor/.local/bin/battery-monitor.sh
+++ battery-monitor/.local/bin/battery-monitor.sh
@@ -5,6 +5,25 @@ low=10
 
 status=$(cat /sys/class/power_supply/BAT0/status)
 capacity=$(cat /sys/class/power_supply/BAT0/capacity)
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo \
+"Usage: $(basename "$0") [-g] [-h]
+
+battery-monitor.sh - Notify when your battery is low or full.
+
+Options:
+  -g, --get   print current battery status and capacity
+  -h, --help  show this help message and exit
+"
+    exit
+fi
+
+if [ "$1" = "-g" ] || [ "$1" = "--get" ]; then
+    echo "Status: $status"
+    echo "Capacity: $capacity%"
+fi
+
 if [ "$status" = "Discharging" ] && [ "$capacity" -le "$low" ]; then
     notify-send "Low battery ($low%)" "Consider charging it and saving your work." -u critical -i battery-caution
 elif [ "$status" = "Full" ]; then
