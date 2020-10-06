0c7910644c71cb07b9b28f824ad01bc9f59416bc
ForgottenUmbrella <forgottenumbrella@tutanota.com>
2020-10-06 12:46:23 +1100

    Revert showing which-key prefix above because it doesn't help

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -469,7 +469,6 @@ to `evil-lookup'. Based on Spacemacs."
                                    "How long to wait before showing keybindings.")
              (which-key-sort-order 'which-key-key-order-alpha
                                    "Sort keybindings alphabetically.")
-             (which-key-show-prefix 'top "Don't obscure menu with keystrokes.")
              :config
              (which-key-mode)
              :general
