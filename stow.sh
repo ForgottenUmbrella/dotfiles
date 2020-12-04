0ca81318cb9553ade4346eaf1a3bec7451000d9e
ForgottenUmbrella <forgottenumbrella@tutanota.com>
2020-12-04 18:01:16 +1100

    Augment stow.sh to forcefully delete orphaned symlinks

--- stow.sh
+++ stow.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
-# Stow in home directory.
+#!/bin/sh
+# Forcefully stow in HOME directory, deleting files somehow not owned.
 
-stow -t ~ "$@"
+stow -t ~ "$@" 2>&1 |
+    awk '/existing target is not owned by stow:/ {print ENVIRON["HOME"] "/" $9}' |
+    xargs -o rm -i && stow -t ~ "$@"
