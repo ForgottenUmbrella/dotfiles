81b4ea16bab3e718dc472f8411eee22342f951c0
ForgottenUmbrella <forgottenumbrella@tutanota.com>
2020-12-30 17:47:29 +1100

    Add another choice to wallplug safebooru tags

--- wallplug/.config/wallplug/config.sh
+++ wallplug/.config/wallplug/config.sh
@@ -24,6 +24,7 @@ command='safebooru_plug "touhou outdoors"'
 #command='safebooru_plug "touhou motsuba"'
 #command='safebooru_plug "touhou ultimate_asuka"'
 #command='safebooru_plug "touhou yoshioka_yoshiko"'
+#command='safebooru_plug "touhou berabou"'
 
 # Alpha of colour scheme.
 alpha=80
