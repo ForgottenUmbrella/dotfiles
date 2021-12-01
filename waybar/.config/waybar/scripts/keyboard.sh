61804859b0746109bb0ffd3f1e6f42a19a3ce92e
Leo Pham <forgottenumbrella@tutanota.com>
2021-12-01 20:03:03 +1100

    Update waybar config
    - Configure all modules
    - Use built-in keyboard module instead of incomplete custom script
    - Move bar to top in preparation for GNOME/eOS rice
    - Move clock to middle for similar reasons
    - Colour workspace background instead of underlining
    - Make config files similar to defaults for easy diffing
    - Minimalism
    - Add interactivity for clicking
    - Fix backlight icons (order was reversed)
    - Support MPRIS in general instead of MPD in particular

--- waybar/.config/waybar/scripts/keyboard.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Output keyboard layout.
-
-# TODO
---
+++ waybar/.config/waybar/scripts/media.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Retrieve MPRIS data.
+# From https://github.com/Alexays/Waybar/wiki/Module:-Custom.
+playerctl -a metadata --format \
+    '{\"text\": \"{{playerName}}: {{artist}} - {{markup_escape(title)}}\",
+    \"tooltip\": \"{{playerName}} : {{markup_escape(title)}}\", \"alt\":
+    \"{{status}}\", \"class\": \"{{status}}\"}' \
+    -F
