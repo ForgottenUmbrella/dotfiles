eb32fa9c0e193b9686f917a837d6938f2093e2ce
Leo Pham <forgottenumbrella@tutanota.com>
2021-12-03 21:02:03 +1100

    Add alt-tab

--- bin/.local/bin/install-arch.sh
+++ bin/.local/bin/install-arch.sh
@@ -286,7 +286,7 @@ paru --sync --refresh --sysupgrade keepassxc dropbox anki firefox-beta antibody-
     rofi-lbonn-wayland rofi-dmenu polybar \
     otf-mplus tamsyn-font-otb tamzen-font ttf-symbola nerd-fonts-fira-code \
     ttf-monoid ttf-font-awesome ttf-ms-fonts \
-    gammastep acpilight mpdris2 kunst-git xorg-xwininfo \
+    gammastep acpilight mpdris2 kunst-git xorg-xwininfo i3-focus-last-git \
     ranger w3m dxvk-bin ashuffle kvantum-qt5 lxappearance \
     fcitx5 fcitx5-configtool fcitx5-gtk fcitx5-qt fcitx5-mozc
 
