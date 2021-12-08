433341ea62362eaaf08a0dd8c9c317fb7d89dee9
Leo Pham <forgottenumbrella@tutanota.com>
2021-12-08 19:43:07 +1100

    Use include files for colour theming in i3 too

--- i3/.config/i3/i3-autostart.sh
+++ i3/.config/i3/i3-autostart.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-# Launch i3-specific autostart programs and configuration.
+# Launch i3-specific autostart programs.
 
 if [ "$XDG_SESSION_DESKTOP" = 'i3' ]
 then
-    i3-msg include "$XDG_CONFIG_HOME/i3/colours"
     pgrep xautolock || xautolock -time 30 -locker 'lock && systemctl suspend-then-hibernate' &
     xmodmap ~/.Xmodmap
     killall -q picom; picom -b --experimental-backends
