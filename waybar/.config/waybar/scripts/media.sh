78d5e900f06dd77946e5c69803c100c1f878811b
Leo Pham <forgottenumbrella@tutanota.com>
2021-12-31 23:04:11 +1100

    Fix waybar media module

--- waybar/.config/waybar/scripts/media.sh
+++ waybar/.config/waybar/scripts/media.sh
@@ -2,7 +2,10 @@
 # Retrieve MPRIS data.
 # From https://github.com/Alexays/Waybar/wiki/Module:-Custom.
 playerctl -a metadata --format \
-    '{\"text\": \"{{playerName}}: {{artist}} - {{markup_escape(title)}}\",
-    \"tooltip\": \"{{playerName}} : {{markup_escape(title)}}\", \"alt\":
-    \"{{status}}\", \"class\": \"{{status}}\"}' \
-    -F
+    '{
+        "text": "{{playerName}}: {{artist}} - {{markup_escape(title)}}",
+        "tooltip": "{{playerName}} : {{markup_escape(title)}}",
+        "alt": "{{status}}",
+        "class": "{{status}}"
+    }' \
+    -F | jq --unbuffered --compact-output
