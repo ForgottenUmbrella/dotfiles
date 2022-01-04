4bc4f459e7ed0dd5459b2912e4a011a606147f96
Leo Pham <forgottenumbrella@tutanota.com>
2022-01-04 21:48:58 +1100

    Change media module formatting

--- waybar/.config/waybar/scripts/media.sh
+++ waybar/.config/waybar/scripts/media.sh
@@ -3,8 +3,8 @@
 # From https://github.com/Alexays/Waybar/wiki/Module:-Custom.
 playerctl -a metadata --format \
     '{
-        "text": "{{playerName}}: {{artist}} - {{markup_escape(title)}}",
-        "tooltip": "{{playerName}} : {{markup_escape(title)}}",
+        "text": "{{artist}} - {{markup_escape(title)}}",
+        "tooltip": "{{markup_escape(title)}} ({{playerName}})",
         "alt": "{{status}}",
         "class": "{{status}}"
     }' \
