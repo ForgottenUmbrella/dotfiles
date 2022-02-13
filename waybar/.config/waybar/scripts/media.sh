3cada3778eb7e905e20f5de7bb782b709c19fa26
Leo Pham <forgottenumbrella@tutanota.com>
2022-02-13 18:24:26 +1100

    Try to avoid redundant symbols in waybar media module

--- waybar/.config/waybar/scripts/media.sh
+++ waybar/.config/waybar/scripts/media.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # Retrieve MPRIS data.
 # From https://github.com/Alexays/Waybar/wiki/Module:-Custom.
-playerctl -a metadata --format \
+playerctl -aF metadata --format \
     '{
         "text": "{{artist}} - {{markup_escape(title)}}",
         "tooltip": "{{markup_escape(title)}} ({{playerName}})",
         "alt": "{{status}}",
         "class": "{{status}}"
-    }' \
-    -F | jq --unbuffered --compact-output
+    }' |
+    sed -e 's/" - /"/' -e 's/ - "/"/' -e 's/" //' |
+    jq --unbuffered --compact-output
