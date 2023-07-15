cea792d8c626ab24fca1d130c3aa701952b7f53f
Leo Pham <wsrgs@proton.me>
2023-07-15 22:36:12 +1000

    Add wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.font = wezterm.font 'monospace'
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.8
+
+-- NVIDIA doesn't work with WezTerm on Wayland.
+-- https://github.com/wez/wezterm/issues/2011
+config.enable_wayland = false
+
+return config
