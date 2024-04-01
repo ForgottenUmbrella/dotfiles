3b5583c53c0deccd529e7f9948f610daf9bd63f1
Leo Pham <wsrgs@proton.me>
2024-04-01 14:54:34 +1100

    Use fallback fonts for wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,9 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.font = wezterm.font 'monospace'
+config.font = wezterm.font_with_fallback {
+  'monospace',
+}
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.8
 
