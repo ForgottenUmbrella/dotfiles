63f96653ebd7be9bb8b9e4e3c93d623762f59f6d
Leo Pham <wsrgs@proton.me>
2024-04-01 14:55:25 +1100

    Add split keybinds for wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,6 +5,28 @@ config.font = wezterm.font_with_fallback {
   'monospace',
 }
 config.hide_tab_bar_if_only_one_tab = true
+config.keys = {
+  {
+    mods = 'CTRL|ALT',
+    key = 's',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    mods = 'CTRL|ALT|SHIFT',
+    key = 's',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    mods = 'CTRL|SHIFT',
+    key = 'w',
+    action = wezterm.action.CloseCurrentPane { confirm = false },
+  },
+  {
+    mods = 'SUPER',
+    key = 'w',
+    action = wezterm.action.CloseCurrentPane { confirm = false },
+  },
+}
 config.window_background_opacity = 0.8
 config.window_close_confirmation = 'NeverPrompt'
 
