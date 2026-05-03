8978fb9c848eb68576e6d3fcf20dcb806849b90e
Leo Pham <wsrgs@proton.me>
2026-05-03 22:04:24 +1000

    Only set wezterm font to monospace on Linux

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,9 +1,11 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.font = wezterm.font_with_fallback {
-  'monospace',
-}
+if not wezterm.target_triple:find 'apple' then
+  config.font = wezterm.font_with_fallback {
+    'monospace',
+  }
+end
 config.hide_tab_bar_if_only_one_tab = true
 config.keys = {
   {
