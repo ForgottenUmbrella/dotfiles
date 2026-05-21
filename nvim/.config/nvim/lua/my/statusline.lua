4b4a7f6b505c66ef50a3b0308bea58f3f9f121fc
Leo <leo@optizmo.com>
2026-05-21 12:17:03 +1000

    Style statusline

--- nvim/.config/nvim/lua/my/statusline.lua
+++ nvim/.config/nvim/lua/my/statusline.lua
@@ -2,34 +2,44 @@
 
 local ctrl_v = vim.api.nvim_replace_termcodes('<C-v>', true, true, true)
 local ctrl_s = vim.api.nvim_replace_termcodes('<C-s>', true, true, true)
-local mode_text_map = {
-  n = 'NORMAL',
-  v = 'VISUAL',
-  V = 'V-LINE',
-  [ctrl_v] = 'V-BLOCK',
-  s = 'SELECT',
-  S = 'S-LINE',
-  [ctrl_s] = 'S-BLOCK',
-  i = 'INSERT',
-  R = 'REPLACE',
-  c = 'COMMAND',
-  r = 'PROMPT',
-  ['!'] = 'SHELL',
-  t = 'TERMINAL',
+local modes = {
+  n = { long = 'NORMAL', short = 'N', hl = '%#MyStatusLineNormal#' },
+  v = { long = 'VISUAL', short = 'V', hl = '%#MyStatusLineVisual#' },
+  V = { long = 'V-LINE', short = 'VL', hl = '%#MyStatusLineVisual#' },
+  [ctrl_v] = { long = 'V-BLOCK', short = 'VB', hl = '%#MyStatusLineVisual#' },
+  s = { long = 'SELECT', short = 'S', hl = '%#MyStatusLineVisual#' },
+  S = { long = 'S-LINE', short = 'SL', hl = '%#MyStatusLineVisual#' },
+  [ctrl_s] = { long = 'S-BLOCK', short = 'SB', hl = '%#MyStatusLineVisual#' },
+  i = { long = 'INSERT', short = 'I', hl = '%#MyStatusLineInsert#' },
+  R = { long = 'REPLACE', short = 'R', hl = '%#MyStatusLineReplace#' },
+  c = { long = 'COMMAND', short = 'C', hl = '%#MyStatusLineCommand#' },
+  r = { long = 'PROMPT', short = 'P', hl = '%#MyStatusLineCommand#' },
+  ['!'] = { long = 'SHELL', short = 'SH', hl = '%#MyStatusLineCommand#' },
+  t = { long = 'TERMINAL', short = 'T', hl = '%#MyStatusLineCommand#' },
 }
-local visual_mode_hl = '%#DiffAdd#'
-local mode_hl_map = {
-  n = '%#Cursor#',
-  v = visual_mode_hl,
-  V = visual_mode_hl,
-  [ctrl_v] = visual_mode_hl,
-  s = visual_mode_hl,
-  S = visual_mode_hl,
-  [ctrl_s] = visual_mode_hl,
-  i = '%#DiffChange#',
-  R = '%#DiffDelete#',
-  c = '%#DiffText#',
-}
+
+vim.api.nvim_create_autocmd({ 'ColorScheme' }, {
+  group = my.augroup,
+  desc = 'Create statusline highlight groups',
+  callback = function()
+    local fg = vim.api.nvim_get_hl(0, { name = 'StatusLine' }).fg
+    local colours = {
+      Normal = 'Type',
+      Visual = 'Special',
+      Insert = 'String',
+      Replace = 'Number',
+      Command = 'Identifier',
+    }
+    for mode, hl_name in pairs(colours) do
+      local bg = vim.api.nvim_get_hl(0, { name = hl_name }).fg
+      vim.api.nvim_set_hl(0, 'MyStatusLine' .. mode, {
+        bg = bg,
+        fg = fg,
+        bold = true,
+      })
+    end
+  end,
+})
 
 local git_status = ''
 if vim.fn.executable 'git' then
@@ -68,59 +78,54 @@ if vim.fn.executable 'git' then
 end
 
 function my.statusline()
-  local file = '%<%f %h%w%m%r'
+  local file = '%f %h%w%m%r'
   local is_active =
     vim.api.nvim_get_current_win() == tonumber(vim.g.actual_curwin)
   if not is_active then
     return table.concat {
-      '%#StatusLine#',
-      ' ',
-      vim.fn.winnr(),
-      ' ',
+      '%#StatusLine# ',
+      vim.fn.winnr(), ' ',
 
-      '%#StatusLineNC#',
-      ' ',
+      '%#StatusLineNC# ',
+      '%<',
       file,
     }
   end
 
+  local width = vim.api.nvim_win_get_width(0)
   local mode = vim.api.nvim_get_mode().mode
-  local mode_text = mode_text_map[mode] or 'UNKNOWN'
-  local mode_hl = mode_hl_map[mode] or '%#IncSearch#'
+  local mode_info = modes[mode] or {
+    long = string.format('UNKNOWN-%s', mode),
+    short = string.format('?%s', mode),
+    hl = '%#MyStatusLineNormal#',
+  }
   local cwd_path = vim.split(vim.fn.getcwd(), '/')
   local progress = table.concat {
     vim.ui.progress_status(),
     vim.opt.busy:get() > 0 and '◐' or '',
   }
-  local ruler = '%l,%c%V %3.p%%'
+  local ruler = width >= 80 and '%6(%l:%c%V%) %3p%%' or '%l:%c%V %p%%'
 
   return table.concat {
-    mode_hl,
-    ' ',
-    mode_text,
-    ' ',
+    mode_info.hl, ' ',
+    width >= 100 and mode_info.long or mode_info.short, ' ',
 
-    '%#StatusLine#',
-    ' ',
-    cwd_path[#cwd_path],
-    ' ',
-    git_status ~= '' and string.format('%s ', git_status) or '',
+    width >= 80 and table.concat {
+      '%#StatusLine# ',
+      cwd_path[#cwd_path], ' ',
+      git_status ~= '' and string.format('%s ', git_status) or '',
+    } or '',
 
-    '%#StatusLineNC#',
-    ' ',
-    file,
-    ' ',
+    '%#StatusLineNC# ',
+    '%<',
+    file, ' ',
     vim.diagnostic.status(),
-
     '%=',
 
-    '%#StatusLine#',
-    progress ~= '' and string.format(' %s ', progress) or '',
+    progress ~= '' and string.format('%#StatusLine# %s ', progress) or '',
 
-    mode_hl,
-    ' ',
-    ruler,
-    ' ',
+    mode_info.hl, ' ',
+    ruler, ' ',
   }
 end
 
